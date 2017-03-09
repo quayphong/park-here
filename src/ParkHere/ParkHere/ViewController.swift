e0527327e66f5b54e190209e702e7d1a1e8f746c
Tan <ngoctan26@gmail.com>
2017-03-09 23:53:13 +0700

    Initialize commit

---
+++ src/ParkHere/ParkHere/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ParkHere
+//
+//  Created by Nguyen Quang Ngoc Tan on 3/9/17.
+//  Copyright © 2017 Nguyen Quang Ngoc Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
